4313b3cc939dd0b6f537cf74e11d51780819907f
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-07-13 10:47:29 -0500

    Solucion de errores

--- angular-crud-frontend/src/app/app.config.ts
+++ angular-crud-frontend/src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+// src/app/app.config.ts
+
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// 1. Importa el proveedor de HttpClient
+import { provideHttpClient } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // 2. Añade el proveedor aquí
+    provideHttpClient() 
   ]
 };
--- djangular/djangocrud/settings.py
+++ djangular/djangocrud/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200", 
+]
