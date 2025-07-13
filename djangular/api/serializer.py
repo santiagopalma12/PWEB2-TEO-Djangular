3cb4a6facf24db9536a8c700855b8ea9456f505d
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-07-13 10:17:01 -0500

    Creación del Serializer

---
+++ djangular/api/serializer.py
@@ -0,0 +1,8 @@
+# api/serializer.py
+from rest_framework import serializers
+from .models import Movie
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'desc', 'year')
